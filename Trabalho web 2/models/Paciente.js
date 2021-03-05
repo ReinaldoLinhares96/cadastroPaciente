cdcf184171dabb01d738547add9bda6df2a8dfe2
ReinaldoLinhares96 <65975532+ReinaldoLinhares96@users.noreply.github.com>
2021-03-05 12:58:30 -0300

    Update Paciente.js

--- Trabalho web 2/models/Paciente.js
+++ Trabalho web 2/models/Paciente.js
@@ -53,25 +53,3 @@ const pacienteSchema = mongoose.Schema({
 
 mongoose.model("pacientes",pacienteSchema)
 
-
-//INSERINDO DADOS DO PACIENTE
-
-//const p1 = mongoose.model("pacientes")  //instancia
-/*
-new p1({
-    nome: "Reinaldo linhares",
-    endereco: "Rua jequitiba",
-    numero: 389,
-    bairro: "aracape",
-    cidade: "Fortaleza",
-    estado: "Ceara",
-    telefone:"(85) 99138-6666",
-    email: "reinaldo_linhres@hotmail.com",
-    dataDeNascimento: "01/02/1996",
-    cpf:"000.000.000-45",
-    diagnostico: "Paciente com febre 38 graus, dor de cabeca e dor no corpo."
-}).save().then(function() {
-    console.log("model criado com sucesso!")
-}).catch(function(erro) {
-    console.log("Falha ao se  criar")
-})*/
